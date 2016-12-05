0ddf985b90e5a5882c090ef1661f3681abab47dc
nathanmargaglio <nathanmargaglio@gmail.com>
2016-12-05 17:21:47 -0500

    Up and Running

--- js/main.js
+++ js/main.js
@@ -10,12 +10,44 @@ sections = [];
 
 window.onload = function(){
     // Init function
+    var start = document.createElement("DIV");
+    start.id = "start";
+    start.style.width = "100%";
+    start.style.display = "table-cell";
+    start.style.textAlign = "center";
+    start.style.position = "absolute";
+    start.style.top = "50%";
+    document.body.appendChild(start);
+
+    var instruct = document.createElement("DIV");
+    start.appendChild(instruct);
+
+    var input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = "Input Positive Integer, Hit Enter..."
+    input.size = "30"
+    input.addEventListener("keyup", function(event){
+        if (event.keyCode == 13){
+            var value = parseInt(input.value);
+            console.log(value);
+            if (!isNaN(value)){
+                generateGrid(Math.abs(value));
+            }else{
+                instruct.innerHTML = "How about a positive integer, eh?<br>"
+            }
+        };
+    });
+    start.appendChild(input);
+};
+
+generateGrid = function(secAmnt){
+    var start = document.getElementById('start');
+    start.remove();
+
     var main = document.createElement("DIV");
     main.style.height = "100%";
     main.style.width = "100%";
     document.body.appendChild(main);
-
-    var secAmnt = 4;
 
     for (y = 0; y < secAmnt; ++y){
         var row = document.createElement("DIV");
@@ -32,7 +64,6 @@ window.onload = function(){
             sec.style.flexDirection = "column";
             sec.style.flex = "1";
             sec.style.overflow = "auto";
-
             sec.onclick = function(){
                 this.style.background = getRandomColor();
             }
