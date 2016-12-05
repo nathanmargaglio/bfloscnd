b04fbd46a72a826d1f9a74b38a030c1ed20dd17d
nathanmargaglio <nathanmargaglio@gmail.com>
2016-12-05 16:05:18 -0500

    Made things worth looking at.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return send_from_directory('html','index.html')
+    return send_from_directory('','index.html')
 
 @app.route("/<file>")
 @app.route("/<dir>/<file>")
--- js/main.js
+++ js/main.js
@@ -7,43 +7,39 @@ function getRandomColor() {
     return color;
 }
 sections = [];
+
 window.onload = function(){
     // Init function
     var main = document.createElement("DIV");
     main.style.height = "100%";
     main.style.width = "100%";
-    document.body.appendChild(main)
+    document.body.appendChild(main);
 
-    var secAmnt = 2;
-    for (x = 0; x < secAmnt; ++x){
-        var sec = document.createElement("DIV");
-        sec.id = "sec" + x.toString();
-        sec.style.width = (100./secAmnt).toString() + "%";
-        sec.style.height = "100%";
-        sec.style.float = "left";
-        sec.style.background = "darkgray";
-        sec.style.display = "flex";
-        sec.style.flexDirection = "column";
-        sec.style.flex = "1";
-        sec.style.overflow = "auto";
-        sections.push(sec);
-        main.appendChild(sec);
+    var secAmnt = 4;
+
+    for (y = 0; y < secAmnt; ++y){
+        var row = document.createElement("DIV");
+        row.style.height = (100./secAmnt).toString() + "%";
+        main.appendChild(row);
+        for (x = 0; x < secAmnt; ++x){
+            var sec = document.createElement("DIV");
+            sec.id = "sec" + (y*secAmnt+x).toString();
+            sec.style.width = (100./secAmnt).toString() + "%";
+            sec.style.height = "100%";
+            sec.style.float = "left";
+            sec.style.background = getRandomColor();
+            sec.style.display = "flex";
+            sec.style.flexDirection = "column";
+            sec.style.flex = "1";
+            sec.style.overflow = "auto";
+
+            sec.onclick = function(){
+                this.style.background = getRandomColor();
+            }
+
+            sections.push(sec);
+            row.appendChild(sec);
+        };
     };
-
-    var table = document.createElement("TABLE");
-    table.class = "table";
-    sections[1].appendChild(table);
-
-    for (y = 0; y < 50; ++y){
-        var row = table.insertRow();
-
-        var cell0 = row.insertCell();
-        var cell1 = row.insertCell();
-        var cell2 = row.insertCell();
-
-        cell0.innerHTML = "HELLO";
-        cell1.innerHTML = ", ";
-        cell2.innerHTML = "WORLD!";
-    }
 };
 
