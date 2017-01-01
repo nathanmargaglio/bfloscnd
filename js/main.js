652bfcf84aaa5bae7af3632d00fdb5f77519ccdf
Nathan Margaglio <nathanmargaglio@gmail.com>
2017-01-01 17:27:52 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-canvas = document.createElement('canvas');
-ctx=canvas.getContext("2d");
-ctx.beginPath();
-ctx.arc(100,75,50,0,2*Math.PI);
-ctx.stroke();
+window.onload = function(){
+  canvas = document.createElement('canvas');
+  ctx=canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(100,75,50,0,2*Math.PI);
+  ctx.stroke();
+};
